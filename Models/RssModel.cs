214c0c9ca4f611d47d2743ef1a346783446db63d
KeltarHelviett <keltarhelviett@gmail.com>
2017-11-08 09:00:42 +1000

    Added news

---
+++ Models/RssModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Univibe_Web.Models
+{
+    public class RssModel
+    {
+        public string Title { set; get; }
+
+        public string Link { set; get; }
+
+        public string Description { set; get; }
+    }
+}
